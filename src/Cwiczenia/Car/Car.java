f78e49ce687bc9a1f0486cf7466f7e7f73be57c9
pawelw89 <pawelwojciechowski89@wp.pl>
2021-02-14 11:19:05 +0100

    Dodano Car

---
+++ src/Cwiczenia/Car/Car.java
@@ -0,0 +1,16 @@
+package Cwiczenia.Car;
+
+public class Car {
+
+    public String marka;
+    public String model;
+    public int rok;
+    public int przebieg;
+
+    public Car(String marka, String model, int rok, int przebieg) {
+        this.marka = marka;
+        this.model = model;
+        this.rok = rok;
+        this.przebieg = przebieg;
+    }
+}
---
+++ src/Cwiczenia/Car/CarTester.java
@@ -0,0 +1,11 @@
+package Cwiczenia.Car;
+
+public class CarTester {
+
+    public static void main(String[] args) {
+
+        Car audi = new Car ("Audi", "A5", 2017, 28000);
+        Car VW = new Car ("Volkswagen", "Golf", 2018, 23000);
+        Car mercedes = new Car ("Mercedes", "Klasa A", 2015, 73000);
+    }
+}
